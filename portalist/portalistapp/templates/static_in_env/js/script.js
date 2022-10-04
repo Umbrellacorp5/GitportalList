c1e307571a1aad1c314412483d29afe06b5634c2
IC <inakiclara2015@hotmail.com>
2022-10-04 18:45:20 -0300

    Codigo totalmente al pedo

--- portalist/portalistapp/templates/static_in_env/js/script.js
+++ portalist/portalistapp/templates/static_in_env/js/script.js
@@ -12,3 +12,31 @@ document.getElementById("btnLoginIP").addEventListener("click", function iniciar
         document.getElementById("incorrectoIP").innerHTML = "El usuario ingresado no existe.";
       }
 })
+
+// JavaScript de Admin //
+
+document.getElementById("btnCrearAlumno").addEventListenerEventListener("click", function IngresarCrearAlumno () {
+  document.getElementById("btnCA").innerHTML = "Has ingresado correctamente."
+
+// JavaScript de Registrar Alumno //
+
+const NombreRA = "a";
+const ApellidoRA = "a"; 
+const CedulaRA = "a";
+const NumeroPadreRA = "a";
+const FotoRA = "a";
+const GrupoRA = "a";
+const EmailRA = "a";
+const UsuarioCiRA = "a";
+const ContraseñaRA = "a";
+
+document.getElementById("btnRegistrarAlumno").addEventListener("click", function RegistrarAlumno() {
+  if (NombreRA === document.getElementById("inputNombreRA").value){                                    //&& ApellidoRA === document.getElementById("inputApellidoRA").value && CedulaRA === document.getElementById("inputCedulaRA").value && NumeroPadreRA === document.getElementById("inputNumeroPadreRA").value && FotoRA === document.getElementById("inputFotoRA").value && GrupoRA === document.getElementById("inputGrupoRA").value && EmailRA === document.getElementById("inputEmailRA").value && UsuarioCiRA === document.getElementById("inputUsuarioCiRA").value && ContraseñaRA === document.getElementById("inputContraseñaRA").value){
+    window.location.replace("//templates/ingresarAlumno.html");
+    document.getElementById("correctoRA").innerHTML = "Te has registrado correctamente.";
+    }
+    else{
+      document.getElementById("incorrectoRA").innerHTML = "No has podido registrarte correctamente."
+    }
+})})
+
